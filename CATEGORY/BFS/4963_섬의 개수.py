fbea769e3685105dce6c0a2a8896e23d9f108327
ejlee <eojin823@gmail.com>
2025-03-11 11:12:50 +0900

    [BFS] 섬의 개수 (풀이 추가)

--- CATEGORY/BFS/4963_섬의 개수.py
+++ CATEGORY/BFS/4963_섬의 개수.py
@@ -1,4 +1,5 @@
 import sys
+from collections import deque
 
 directions = [(-1, -1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0)]
 sys.setrecursionlimit(10 ** 6)
@@ -34,4 +35,42 @@ def solve():
         line += 1
 
 
-solve()
+# solve()
+
+
+def bfs(grid, x, y, w, h):
+    queue = deque([(x, y)])
+    grid[x][y] = 0
+
+    while queue:
+        cx, cy = queue.popleft()
+        for dx, dy in directions:
+            nx, ny = cx + dx, cy + dy
+            if 0 <= nx < h and 0 <= ny < w and grid[nx][ny] == 1:
+                queue.append((nx, ny))
+                grid[nx][ny] = 0
+
+
+def sol():
+    read = sys.stdin.read().strip().splitlines()
+    index = 0
+
+    while index < len(read):
+        w, h = map(int, read[index].split())
+        if w == 0 and h == 0:
+            break
+
+        grid = [list(map(int, read[i].split())) for i in range(index + 1, index + 1 + h)]
+        index += h + 1
+
+        islands = 0
+        for x in range(h):
+            for y in range(w):
+                if grid[x][y] == 1:
+                    bfs(grid, x, y, w, h)
+                    islands += 1
+
+        sys.stdout.write(str(islands) + '\n')
+
+
+sol()
