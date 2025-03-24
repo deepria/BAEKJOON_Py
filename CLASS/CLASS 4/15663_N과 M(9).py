a94431f280a260874fb307dae7b1e167e83f5a50
ejlee <eojin823@gmail.com>
2025-03-24 09:55:59 +0900

    [CLASS 4] N과 M(9)

---
+++ CLASS/CLASS 4/15663_N과 M(9).py
@@ -0,0 +1,30 @@
+import sys
+
+
+def backtrack(depth, n, m, nums, current, used, results):
+    if depth == m:
+        results.append(current[:])
+        return
+
+    last = -1
+    for i in range(n):
+        if not used[i] and nums[i] != last:
+            used[i] = True
+            current.append(nums[i])
+            backtrack(depth + 1, n, m, nums, current, used, results)
+            current.pop()
+            used[i] = False
+            last = nums[i]
+
+
+def sol():
+    read = sys.stdin.read().splitlines()
+    n, m = map(int, read[0].split())
+    nums = sorted(list(map(int, read[1].split())))
+    results = []
+    backtrack(0, n, m, nums, [], [False] * n, results)
+    for result in results:
+        print(' '.join(map(str, result)))
+
+
+sol()
