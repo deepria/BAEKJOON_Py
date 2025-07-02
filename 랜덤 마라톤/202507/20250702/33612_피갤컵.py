b96be8452e9802b0837dc9436834f457781b99f9
ejlee <eojin823@gmail.com>
2025-07-02 16:31:00 +0900

    [랜덤 마라톤] 20250702 A

---
+++ 랜덤 마라톤/202507/20250702/33612_피갤컵.py
@@ -0,0 +1,12 @@
+n = input()
+match n:
+    case '1':
+        print('2024 8')
+    case '2':
+        print('2025 3')
+    case '3':
+        print('2025 10')
+    case '4':
+        print('2026 5')
+    case '5':
+        print('2026 12')
